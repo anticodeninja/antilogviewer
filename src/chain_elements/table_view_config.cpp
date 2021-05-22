a706ca531eae6ac637bd24f8a36b4d7d5861c271
anticode.ninja <me@anticode.ninja>
2021-05-22 16:03:39 +0300

    Update license headers

--- src/chain_elements/table_view_config.cpp
+++ src/chain_elements/table_view_config.cpp
@@ -1,7 +1,7 @@
 // This Source Code Form is subject to the terms of the
 // Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
 // with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-// Copyright 2019 Artem Yamshanov, me [at] anticode.ninja
+// Copyright 2019-2021 Artem Yamshanov, me [at] anticode.ninja
 
 #include "table_view_config.h"
 
